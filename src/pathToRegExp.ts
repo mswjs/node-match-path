0ef18144d405876bfffa28c242055c69edfb1e55
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-10 12:36:38 +0100

    Ignores trailing slash

--- src/pathToRegExp.ts
+++ src/pathToRegExp.ts
@@ -1,13 +1,18 @@
 /**
- * Converts a string path to regular expression.
+ * Converts a string path to a Regular Expression.
  * Transforms path parameters into named RegExp groups.
  */
 export const pathToRegExp = (path: string): RegExp => {
   const pattern = path
+    // Escape literal dots
     .replace(/\./g, '\\.')
+    // Escape literal slashes
     .replace(/\//g, '/')
+    // Ignore trailing slashes
+    .replace(/\/+$/, '')
     .replace('*', '.+?')
     .replace(/:([^\d]\w+(?=\/|$))/g, (_, match) => `(?<${match}>.+?(?=\/|$))`)
+    .concat('\\/?')
 
   return new RegExp(pattern, 'g')
 }
